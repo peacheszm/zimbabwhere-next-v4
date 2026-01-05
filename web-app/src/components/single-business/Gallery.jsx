4ab1ead0dacfc4952a0d56c24ce2929c663643c8
Ninjas-for-hire <ninjas@ninjasforhire.co.za>
2026-01-05 23:25:43 +0200

    Single Business

--- web-app/src/components/single-business/Gallery.jsx
+++ web-app/src/components/single-business/Gallery.jsx
@@ -1,10 +1,20 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Thumbs } from "swiper/modules";
-
 import Image from "next/image";
 
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { IconChevronRight, IconChevronLeft } from "@tabler/icons-react";
+
 export default function Gallery({ post }) {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  const paginationRef = useRef(null);
+
   return (
     <>
       {Array.isArray(post.acf?.uploads) && post.acf.uploads.length > 0 && (
@@ -12,11 +22,22 @@ export default function Gallery({ post }) {
           {/* Image Gallery Swiper */}
           {post.acf.uploads.filter((file) => file.file.subtype !== "pdf")
             .length > 0 && (
-            <div className="gallery_swiper_container">
+            <div className="gallery_slider">
               <Swiper
                 modules={[Navigation, Pagination, Thumbs]}
-                navigation={true}
-                pagination={{ clickable: true }}
+                navigation={{
+                  prevEl: prevRef.current,
+                  nextEl: nextRef.current,
+                }}
+                pagination={{
+                  el: paginationRef.current,
+                  clickable: true,
+                }}
+                onBeforeInit={(swiper) => {
+                  swiper.params.navigation.prevEl = prevRef.current;
+                  swiper.params.navigation.nextEl = nextRef.current;
+                  swiper.params.pagination.el = paginationRef.current;
+                }}
                 spaceBetween={10}
                 slidesPerView={1}
                 breakpoints={{
@@ -51,6 +72,21 @@ export default function Gallery({ post }) {
                     </SwiperSlide>
                   ))}
               </Swiper>
+
+              {/* Custom Navigation & Pagination - Outside Swiper */}
+              <div className="pagination_wrapper">
+                <button
+                  ref={prevRef}
+                  className="prev"
+                  aria-label="Previous slide"
+                >
+                  <IconChevronLeft />
+                </button>
+                <div ref={paginationRef} className="navigation"></div>
+                <button ref={nextRef} className="next" aria-label="Next slide">
+                  <IconChevronRight />
+                </button>
+              </div>
             </div>
           )}
 
