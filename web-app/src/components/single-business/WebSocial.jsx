e46828dc61211c852604e9649dfae68807b1c27e
Ninjas-for-hire <ninjas@ninjasforhire.co.za>
2026-01-23 16:38:51 +0200

    Updates

--- web-app/src/components/single-business/WebSocial.jsx
+++ web-app/src/components/single-business/WebSocial.jsx
@@ -1,12 +1,38 @@
+import Image from "next/image";
 import {
   IconBrandFacebook,
   IconBrandTwitter,
   IconBrandYoutube,
   IconFileText,
+  IconWorldWww,
 } from "@tabler/icons-react";
 export default function WebSocial({ post }) {
   return (
     <div className="web_and_social">
+      <div className="business_logo">
+        {post._embedded?.["wp:featuredmedia"] ? (
+          <Image
+            src={post._embedded["wp:featuredmedia"][0].source_url}
+            alt={post.title?.rendered}
+            width={200}
+            height={200}
+          />
+        ) : post.acf?.logo ? (
+          <Image
+            src={post.acf.logo.url}
+            alt={post.title?.rendered}
+            width={200}
+            height={200}
+          />
+        ) : (
+          <Image
+            src="/img/placeholder.png"
+            alt="Placeholder"
+            width={200}
+            height={200}
+          />
+        )}
+      </div>
       {/* Visit website */}
       {post.acf?.business_website && (
         <div className="business_website btn_group">
@@ -17,7 +43,7 @@ export default function WebSocial({ post }) {
             rel="noopener noreferrer"
           >
             <span className="icon">
-              <IconFileText size={20} />
+              <IconWorldWww size={20} />
             </span>
             Visit Website
           </a>
