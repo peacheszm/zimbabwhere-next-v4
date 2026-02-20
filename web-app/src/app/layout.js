86ab798c7be537a7fccd670402394ecd3fc8d07b
Ninjas-for-hire <ninjas@ninjasforhire.co.za>
2026-02-20 22:45:06 +0200

    Added google tags

--- web-app/src/app/layout.js
+++ web-app/src/app/layout.js
@@ -60,6 +60,18 @@ export default function RootLayout({ children }) {
         </noscript>
         <ClientWrapper>{children}</ClientWrapper>
         <Analytics />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XJEZS0JHVS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XJEZS0JHVS');
+          `}
+        </Script>
       </body>
     </html>
   );
