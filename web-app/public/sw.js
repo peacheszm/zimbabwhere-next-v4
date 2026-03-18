2c5c7ee6c3e40104a1651e26d04a3ca1acb49899
Ninjas-for-hire <ninjas@ninjasforhire.co.za>
2026-03-18 21:36:29 +0200

    Sw cache update

--- web-app/public/sw.js
+++ web-app/public/sw.js
@@ -1,29 +1,29 @@
-const CACHE_NAME = 'zimbabwhere-v1';
+const CACHE_NAME = "zimbabwhere-v1.0.1";
 const ASSETS_TO_CACHE = [
-  '/',
-  '/manifest.json',
-  '/img/icon-192.png',
-  '/img/icon-512.png',
-  '/img/zimbabwhere-logo.png'
+  "/",
+  "/manifest.json",
+  "/img/icon-192.png",
+  "/img/icon-512.png",
+  "/img/zimbabwhere-logo.png",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -31,8 +31,8 @@ self.addEventListener('activate', (event) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
